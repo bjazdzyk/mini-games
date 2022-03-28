e64634765ba10f4357c97b280217bed002d4c9d0
Bartosz Jażdżyk <bartek.jazdzyk.007@gmail.com>
2022-03-28 19:43:30 +0200

    snake - resizable board

--- docs/games/snake/main.js
+++ docs/games/snake/main.js
@@ -8,9 +8,35 @@ document.body.appendChild(app.view);
 app.renderer.backgroundColor = 0x0faaf0
 
 
+const rows = 10
+const cols = 15
+const margin = 1
+let cellSize = Math.min(_W/(cols+margin*2), _H/(rows+margin*2))
+let width = cellSize*cols
+let height = cellSize*rows
+const T = {}
 
 
-const loop =()=>{
+const vec2 = (x, y)=>{
+	return `${x}:${y}`
+}
+
+const render = ()=>{
+	board.clear()
+	board.beginFill(0xeeeeee)
+	board.lineStyle(5, 0x000000)
+	board.drawRect(0, 0, width, height)
+	const offsetX = (_W-width)/2
+	const offsetY = (_H-height)/2
+	board.x = offsetX
+	board.y = offsetY
+}
+
+const board = new PIXI.Graphics()
+app.stage.addChild(board)
+render()
+
+const loop = ()=>{
 
 
 }
@@ -20,5 +46,10 @@ loop()
 window.addEventListener('resize', ()=>{
 	_W = window.innerWidth
 	_H = window.innerHeight
+	cellSize = Math.min(_W/(cols+margin*2), _H/(rows+margin*2))
+	width = cellSize*cols
+	height = cellSize*rows
 	app.renderer.resize(_W, _H)
+	board.clear()
+	render()
 })
