9918bb5bbcfdbb72bd515cc00ee334dedf046699
Bartosz Jażdżyk <bartek.jazdzyk.007@gmail.com>
2022-03-28 19:18:57 +0200

    snake - scene setup

--- docs/games/snake/main.js
+++ docs/games/snake/main.js
@@ -1,1 +1,24 @@
-alert("Snake")
+
+let _W = window.innerWidth
+let _H = window.innerHeight
+
+let app = new PIXI.Application({ width: _W, height: _H });
+document.body.appendChild(app.view);
+
+app.renderer.backgroundColor = 0x0faaf0
+
+
+
+
+const loop =()=>{
+
+
+}
+loop()
+
+//window resize
+window.addEventListener('resize', ()=>{
+	_W = window.innerWidth
+	_H = window.innerHeight
+	app.renderer.resize(_W, _H)
+})
